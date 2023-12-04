506d8416716bd143bc63213f19f525e796859ea0
Nora Heaphy <nora.heaphy@yale.edu>
2023-12-04 11:40:22 -0500

    windowed ABBA-BABA

--- project/ABBA-BABA.R
+++ project/ABBA-BABA.R
@@ -1,3 +1,5 @@
 # read in ABBA-BABA results
-result <- read.csv("result.Observed.txt", header = T, sep = "\t", na.strings = "")
-result_transrem <- read.csv("result.TransRem.txt", header = T, sep = "\t", na.strings = "")
+overall_result <- read.csv("result.Observed.txt", header = T, sep = "\t", na.strings = "")
+window_result10k <- read.csv("window_10k_result.Observed.txt", header = T, sep = "\t", na.strings = "")
+window_result5k <- read.csv("window_5k_result.Observed.txt", header = T, sep = "\t", na.strings = "")
+window_result1k <- read.csv("window_1k_result.Observed.txt", header = T, sep = "\t", na.strings = "")
